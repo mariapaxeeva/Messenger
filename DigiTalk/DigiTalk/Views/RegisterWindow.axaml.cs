13452b00ccca932d902b088f70eca1168fcbe923
GitUser <mashapacseeva@gmail.com>
2025-05-12 00:34:30 +0700

    Structure DigiTalk (Client)

---
+++ DigiTalk/DigiTalk/Views/RegisterWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DigiTalk.Views;
+
+public partial class RegisterWindow : Window
+{
+    public RegisterWindow()
+    {
+        InitializeComponent();
+    }
+}
