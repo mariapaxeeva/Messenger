ea4a734605b6148ab724e12dbc197baa9b902961
GitUser <mashapacseeva@gmail.com>
2025-05-16 22:29:29 +0700

    Create model of Message

--- DigiTalk/DigiTalk/Models/Message.cs
+++ DigiTalk/DigiTalk/Models/Message.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Сообщение
+using System;
 
-namespace DigiTalk.Models
+public class Message
 {
-    internal class Message
-    {
-    }
+    public int Id { get; set; } // Идентификатор
+    public string Sender { get; set; } // Отправитель
+    public string Recipient { get; set; } // Получатель
+    public string Content { get; set; } // Содержание
+    public DateTime Timestamp { get; set; } // Временная метка
+    public bool IsGroupMessage { get; set; } // true - отправлено группе, false - личное
+    public bool IsOwnMessage { get; set; } // true - отправлено текущим пользователем, false - другим
+    public bool IsDeleted { get; set; } // true - сообщение удалено из чата
 }
