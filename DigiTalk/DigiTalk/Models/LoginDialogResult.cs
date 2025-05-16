9e637f60d9eb29374cdc78726c6e6b16ce661820
GitUser <mashapacseeva@gmail.com>
2025-05-16 22:41:11 +0700

    Create model of result log in

--- DigiTalk/DigiTalk/Models/LoginDialogResult.cs
+++ DigiTalk/DigiTalk/Models/LoginDialogResult.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// Запрос на вход в мессенджер
 namespace DigiTalk.Models
 {
-    internal class LoginDialogResult
+    public class LoginDialogResult
     {
+        public bool Result { get; set; } // true - успех, false - ошибка
+        public string Username { get; set; } // Имя пользователя
+        public string Password { get; set; } // Пароль
+        public bool IsRegister { get; set; } // true - запрос является регистрацией, false - авторизацией
     }
 }
