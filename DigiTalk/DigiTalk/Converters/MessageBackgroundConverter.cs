50e1aa15280091cc9a65af9a7c1c60bd0139c967
GitUser <mashapacseeva@gmail.com>
2025-05-18 00:26:16 +0700

    Realize MessageBackgroundConverter <client>

--- DigiTalk/DigiTalk/Converters/MessageBackgroundConverter.cs
+++ DigiTalk/DigiTalk/Converters/MessageBackgroundConverter.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Конвертер для фона сообщения
+// Если сообщение в чате принадлежит текущему пользователю, конвертер возвращает светло-розовую кисть, иначе - белую
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
 
 namespace DigiTalk.Converters
 {
-    internal class MessageBackgroundConverter
+    public class MessageBackgroundConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isOwnMessage)
+            {
+                return isOwnMessage ? new SolidColorBrush(Color.Parse("#e5ccff")) : new SolidColorBrush(Color.Parse("#ffe3fe"));
+            }
+            return Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
