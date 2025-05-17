8f3e7204eab7f91e8d99b36ee1c9cbf446856c5c
GitUser <mashapacseeva@gmail.com>
2025-05-18 00:25:39 +0700

    Realize SenderColorConverter <client>

--- DigiTalk/DigiTalk/Converters/SenderColorConverter.cs
+++ DigiTalk/DigiTalk/Converters/SenderColorConverter.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Конвертер для цвета шрифта отправителя сообщения
+// Если сообщение в чате принадлежит текущему пользователю, конвертер возвращает розовую кисть, иначе - черную
+
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
 
 namespace DigiTalk.Converters
 {
-    internal class SenderColorConverter
+    public class SenderColorConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isOwnMessage)
+            {
+                return isOwnMessage ? new SolidColorBrush(Color.Parse("#a37ddb")) : new SolidColorBrush(Color.Parse("#f59eff"));
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
